da64fe59f913833b8c4265e1c9e952e9cb1f9e70
kartik5800 <jinalmathukiya74@gmail.com>
2022-03-23 19:35:29 -0700

    modified

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React1
+          Learn React123465789
         </a>
       </header>
     </div>
